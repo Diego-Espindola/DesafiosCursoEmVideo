55a3c27da6a6ba6eca564b243fe029349dfddb94
Diego Espindola <diegoespindola9404@gmail.com>
2021-08-25 16:22:31 -0300

    Continuando os desafios

---
+++ CursoPython013/Desafio53.py
@@ -0,0 +1,20 @@
+#Crie um programa que leia uma frase qualquer e diga se ela é um palíndromo, desconsiderando os espaços.
+#print('**********************************************************')
+#print('Este programa descobre se a frase digitada é um palíndromo')
+#print('**********************************************************')
+
+#frase = input('Digite a frase: ').replace(" ", "")
+
+def teste_palindromo(frase):
+        
+    frase = (frase.replace(" ", "")).lower()
+
+    frase_invertida = ''
+
+    for c in frase[::-1]:
+        frase_invertida += c
+
+    if(frase == frase_invertida):
+        print('Essa frase é um palíndromo')
+    else:
+        print('Essa frase não é um palíndromo')
